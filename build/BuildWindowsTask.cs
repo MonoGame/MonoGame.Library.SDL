63eab5671f03a5b431a928793f86558c31ed71c7
Tom Spilman <tom@sickheadgames.com>
2024-08-25 10:43:42 -0500

    Include the SDL2 static lib as we transition to the new MG native backends.

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -14,10 +14,11 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         var buildDir = "sdl/build";
         context.CreateDirectory(buildDir);
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A x64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
-        context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release" });
+        context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release /p:Platform=x64" });
 
         // Copy artifact
         context.CreateDirectory(context.ArtifactsDir);
         context.CopyFile("sdl/build/Release/SDL2.dll", $"{context.ArtifactsDir}/SDL2.dll");
+        context.CopyFile("sdl/build/Release/SDL2-static.lib", $"{context.ArtifactsDir}/SDL2-static.lib");
     }
 }
