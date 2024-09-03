78bbd75ce02ce7e64bc4dc9c53e820b90f1470e7
Dean Ellis <dellis1972@googlemail.com>
2024-09-03 17:00:42 +0100

    Update buildscriopts. Add windows ARM64 support

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -11,13 +11,22 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         // Build
-        var buildDir = "sdl/build";
+        var buildDir = "sdl/build_x64";
         context.CreateDirectory(buildDir);
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A x64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
         context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release" });
 
         // Copy artifact
         context.CreateDirectory(context.ArtifactsDir);
-        context.CopyFile("sdl/build/Release/SDL2.dll", $"{context.ArtifactsDir}/SDL2.dll");
+        context.CopyFile("sdl/build_x64/Release/SDL2.dll", $"{context.ArtifactsDir}/win-x64/SDL2.dll");
+
+        buildDir = "sdl/build_arm64";
+        context.CreateDirectory(buildDir);
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A ARM64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
+        context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release" });
+
+        // Copy artifact
+        context.CreateDirectory(context.ArtifactsDir);
+        context.CopyFile("sdl/build_arm64/Release/SDL2.dll", $"{context.ArtifactsDir}/win-arm64/SDL2.dll");
     }
 }
