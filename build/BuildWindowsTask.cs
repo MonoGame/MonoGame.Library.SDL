2bb67391f9182044863e1a3304b14b053e4958ff
Dean Ellis <dellis1972@googlemail.com>
2024-11-28 18:19:50 +0000

    Update buildscripts. Add windows ARM64 support (#4)
    * Update buildscriopts. Add windows ARM64 support
    
    * Bump NuGet Packages
    
    * Create the directories
    
    * Bump buildscripts tp e1aac69
    
    * Bump SDL to release-2.30.7
    
    * Bump buildscripts to 48247d7
    
    * Bump buildscripts to 388432d
    
    * Update Build.csproj
    
    * Update Build.csproj
    
    * bump buildscripts
    
    * Bump framework version
    
    * Bump framework version

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -11,13 +11,24 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         // Build
-        var buildDir = "sdl/build";
+        var buildDir = "sdl/build_x64";
         context.CreateDirectory(buildDir);
+        context.CreateDirectory($"{context.ArtifactsDir}/win-x64");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A x64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
         context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release" });
 
         // Copy artifact
         context.CreateDirectory(context.ArtifactsDir);
-        context.CopyFile("sdl/build/Release/SDL2.dll", $"{context.ArtifactsDir}/SDL2.dll");
+        context.CopyFile("sdl/build_x64/Release/SDL2.dll", $"{context.ArtifactsDir}/win-x64/SDL2.dll");
+
+        buildDir = "sdl/build_arm64";
+        context.CreateDirectory(buildDir);
+        context.CreateDirectory($"{context.ArtifactsDir}/win-arm64");
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "-A ARM64 -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ../" });
+        context.StartProcess("msbuild", new ProcessSettings { WorkingDirectory = buildDir, Arguments = "SDL2.sln /p:Configuration=Release" });
+
+        // Copy artifact
+        context.CreateDirectory(context.ArtifactsDir);
+        context.CopyFile("sdl/build_arm64/Release/SDL2.dll", $"{context.ArtifactsDir}/win-arm64/SDL2.dll");
     }
 }
